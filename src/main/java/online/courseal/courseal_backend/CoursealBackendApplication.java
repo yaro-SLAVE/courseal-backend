801ef2fad88ac7308ab92c52e051b83888200ac5
ElectronApps <klimchikeg@gmail.com>
2024-02-24 22:19:40 +0800

    Added docker scripts

--- src/main/java/online/courseal/courseal_backend/CoursealBackendApplication.java
+++ src/main/java/online/courseal/courseal_backend/CoursealBackendApplication.java
@@ -1,13 +1,21 @@
 package online.courseal.courseal_backend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CoursealBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoursealBackendApplication.class, args);
 	}
 
+	@GetMapping("/api/courseal-info")
+	public String apiInfo() {
+		return "TODO: actual api info";
+	}
 }
