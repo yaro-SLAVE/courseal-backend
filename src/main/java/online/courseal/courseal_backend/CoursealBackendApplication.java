28711635a8b1bf33546a496e0b6243f9bc826596
yaro-SLAVE <85624526+yaro-SLAVE@users.noreply.github.com>
2024-02-24 20:23:19 +0800

    Initial commit

---
+++ src/main/java/online/courseal/courseal_backend/CoursealBackendApplication.java
@@ -0,0 +1,13 @@
+package online.courseal.courseal_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursealBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursealBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/courseal/courseal_backend/CoursealBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package online.courseal.courseal_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursealBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
