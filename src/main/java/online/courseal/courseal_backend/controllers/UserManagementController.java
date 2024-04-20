ee2d45ac43cd3e9ab0487cf3d462eadbc89e5d2e
yaro-SLAVE <85624526+yaro-SLAVE@users.noreply.github.com>
2024-04-20 16:17:58 +0800

    Updated UserManagementController

--- src/main/java/online/courseal/courseal_backend/controllers/UserManagementController.java
+++ src/main/java/online/courseal/courseal_backend/controllers/UserManagementController.java
@@ -3,18 +3,28 @@ package online.courseal.courseal_backend.controllers;
 import online.courseal.courseal_backend.configs.ServerConfig;
 import online.courseal.courseal_backend.errors.exceptions.IncorrectUsertagException;
 import online.courseal.courseal_backend.errors.exceptions.RegistrationEnabledException;
+import online.courseal.courseal_backend.errors.exceptions.UserNotFoundException;
+import online.courseal.courseal_backend.models.CourseEnrollment;
+import online.courseal.courseal_backend.models.CourseMaintainer;
 import online.courseal.courseal_backend.models.User;
 import online.courseal.courseal_backend.requests.ChangingNameRequest;
 import online.courseal.courseal_backend.requests.ChangingPasswordRequest;
 import online.courseal.courseal_backend.responses.MessageResponse;
 import online.courseal.courseal_backend.requests.RegisterRequest;
 import online.courseal.courseal_backend.repositories.UserRepository;
+import online.courseal.courseal_backend.responses.UserInfoResponse;
+import online.courseal.courseal_backend.responses.data.CoursesEnrollmentsData;
+import online.courseal.courseal_backend.responses.data.CoursesMaintainersData;
+import online.courseal.courseal_backend.services.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -63,9 +73,42 @@ public class UserManagementController {
 
     @GetMapping
     public ResponseEntity<?> getInfo(){
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Optional<User> users = userRepository.findByUserTag(userDetails.getUserTag());
 
+        int xp = 0;
+        for (CourseEnrollment courseEnrollment: users.get().getCourseEnrollments()) {
+            xp += courseEnrollment.getXp();
+        }
 
-        return null;
+        ArrayList<CoursesEnrollmentsData> coursesEnrollmentsDataList = new ArrayList<>();
+        for (CourseEnrollment courseEnrollment: users.get().getCourseEnrollments()) {
+            coursesEnrollmentsDataList.add(new CoursesEnrollmentsData(
+                    courseEnrollment.getCourse().getCourseId(),
+                    courseEnrollment.getCourse().getCourseName(),
+                    courseEnrollment.getXp()
+            ));
+        }
+
+        ArrayList<CoursesMaintainersData> coursesMaintainersDataList = new ArrayList<>();
+        for (CourseMaintainer courseMaintainer: users.get().getCourseMaintainers()) {
+            coursesMaintainersDataList.add(new CoursesMaintainersData(
+                    courseMaintainer.getCourse().getCourseId(),
+                    courseMaintainer.getCourse().getCourseName(),
+                    courseMaintainer.getPermissions()
+            ));
+        }
+
+        return ResponseEntity.ok(new UserInfoResponse(
+                users.get().getUserTag(),
+                users.get().getUserName(),
+                users.get().getDateCreated(),
+                users.get().isCanCreate(),
+                xp,
+                "",
+                coursesEnrollmentsDataList,
+                coursesMaintainersDataList
+        ));
     }
 
     @PutMapping("/username")
