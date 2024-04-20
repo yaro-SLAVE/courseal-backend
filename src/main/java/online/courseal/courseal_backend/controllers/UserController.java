fa29a64e543feaef1ef1859b90ace65b06d5543d
yaro-SLAVE <85624526+yaro-SLAVE@users.noreply.github.com>
2024-04-20 11:54:56 +0800

    Updated user/{usertag}

--- src/main/java/online/courseal/courseal_backend/controllers/UserController.java
+++ src/main/java/online/courseal/courseal_backend/controllers/UserController.java
@@ -1,18 +1,69 @@
 package online.courseal.courseal_backend.controllers;
 
+import online.courseal.courseal_backend.errors.exceptions.UserNotFoundException;
+import online.courseal.courseal_backend.models.CourseEnrollment;
+import online.courseal.courseal_backend.models.CourseMaintainer;
+import online.courseal.courseal_backend.models.User;
+import online.courseal.courseal_backend.models.UserActivity;
+import online.courseal.courseal_backend.repositories.UserRepository;
+import online.courseal.courseal_backend.responses.UserInfoResponse;
+import online.courseal.courseal_backend.responses.data.CoursesEnrollmentsData;
+import online.courseal.courseal_backend.responses.data.CoursesMaintainersData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class UserController {
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/{usertag}")
-    public ResponseEntity<?> getUserInfo(){
-        return null;
+    public ResponseEntity<?> getUserInfo(@PathVariable("usertag") String userTag){
+        Optional<User> users = userRepository.findByUserTag(userTag);
+
+        if (users.isEmpty()){
+            throw new UserNotFoundException();
+        }
+
+        int xp = 0;
+        for (UserActivity userActivity: users.get().getUserActivities()) {
+            xp += userActivity.getXp();
+        }
+
+        ArrayList<CoursesEnrollmentsData> coursesEnrollmentsDataList = new ArrayList<>();
+        for (CourseEnrollment courseEnrollment: users.get().getCourseEnrollments()) {
+            coursesEnrollmentsDataList.add(new CoursesEnrollmentsData(
+                    courseEnrollment.getCourse().getCourseId(),
+                    courseEnrollment.getXp()
+            ));
+        }
+
+        ArrayList<CoursesMaintainersData> coursesMaintainersDataList = new ArrayList<>();
+        for (CourseMaintainer courseMaintainer: users.get().getCourseMaintainers()) {
+            coursesMaintainersDataList.add(new CoursesMaintainersData(
+                    courseMaintainer.getCourse().getCourseId(),
+                    courseMaintainer.getPermissions()
+            ));
+        }
+
+        return ResponseEntity.ok(new UserInfoResponse(
+                users.get().getUserTag(),
+                users.get().getUserName(),
+                users.get().getDateCreated(),
+                users.get().isCanCreate(),
+                xp,
+                "",
+                coursesEnrollmentsDataList,
+                coursesMaintainersDataList
+        ));
     }
 
     @GetMapping("/{usertag}/activity")
