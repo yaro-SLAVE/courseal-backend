2e87802383e7ee983d6df326872288ef959d3726
yaro-SLAVE <85624526+yaro-SLAVE@users.noreply.github.com>
2024-04-21 18:23:37 +0800

    Added requests and response for create task

--- src/main/java/online/courseal/courseal_backend/requests/RegisterRequest.java
+++ src/main/java/online/courseal/courseal_backend/requests/RegisterRequest.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
 public class RegisterRequest {
     private String usertag;
     private String username;
--- src/main/java/online/courseal/courseal_backend/requests/TaskCreatingRequest.java
+++ src/main/java/online/courseal/courseal_backend/requests/TaskCreatingRequest.java
@@ -1,4 +1,15 @@
 package online.courseal.courseal_backend.requests;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import online.courseal.courseal_backend.requests.data.task.TaskRequestData;
+
+import java.util.List;
+
+@Getter
 public class TaskCreatingRequest {
+    @JsonProperty("task_name")
+    private String taskName;
+    @JsonProperty("task")
+    private List<TaskRequestData> dataList;
 }
