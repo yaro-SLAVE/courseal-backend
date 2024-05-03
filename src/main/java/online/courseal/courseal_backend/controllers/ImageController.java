0a3c1edc54813369e90b699d581ba11d30306f8c
yaro-SLAVE <85624526+yaro-SLAVE@users.noreply.github.com>
2024-05-03 20:49:14 +0800

    Added classes for images

---
+++ src/main/java/online/courseal/courseal_backend/controllers/ImageController.java
@@ -0,0 +1,39 @@
+package online.courseal.courseal_backend.controllers;
+
+import online.courseal.courseal_backend.coursedata.editorjs.data.EditorJSImageFile;
+import online.courseal.courseal_backend.responses.records.EditorJSImageResponse;
+import online.courseal.courseal_backend.services.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/static")
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class ImageController {
+    @Autowired
+    ImageService imageService;
+
+    @PostMapping("editorjsimage")
+    public EditorJSImageResponse uploadEditorJSImage(@RequestHeader("Host") String host, @RequestParam("image") MultipartFile file) {
+        return imageService.saveImage(file)
+            .map(uuid -> new EditorJSImageResponse(
+                1,
+                new EditorJSImageFile(
+                    "http://" + host + "/api/static/image/" + uuid
+                )
+            ))
+            .orElse(new EditorJSImageResponse(
+                0,
+                null
+            ));
+    }
+
+    @GetMapping(value = "image/{uuid}", produces = MediaType.IMAGE_PNG_VALUE)
+    public Resource getImage(@PathVariable String uuid) {
+        return imageService.getImage(uuid);
+    }
+
+}
---
+++ src/main/java/online/courseal/courseal_backend/services/ImageService.java
@@ -0,0 +1,47 @@
+package online.courseal.courseal_backend.services;
+
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ImageService {
+
+    public Optional<String> saveImage(MultipartFile file) {
+        String contentType = file.getContentType();
+        if (contentType != null && (contentType.equals("image/png") || contentType.equals("image/jpeg"))) {
+            String uuid = UUID.randomUUID().toString();
+
+            try {
+                Files.createDirectories(Paths.get("data", "images"));
+
+                BufferedImage image = ImageIO.read(file.getInputStream());
+                ImageIO.write(image, "png", new File("data/images/" + uuid));
+                return Optional.of(uuid);
+            } catch (IOException e) {
+                return Optional.empty();
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public ByteArrayResource getImage(String uuid) {
+        var path = Paths.get("data", "images", uuid);
+        try {
+            return new ByteArrayResource(Files.readAllBytes(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
